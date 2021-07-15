89977147e02270b8ba6f54d83a3ce5396eb14320
Vitaly <vitalic.web@gmail.com>
2021-07-15 15:54:17 +0200

    Создан компонент ряда таблицы

---
+++ src/components/TableRow.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr>
+    <td>{{userName}}</td>
+    <td>{{date}}</td>
+    <td>{{year}}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'TableRow',
+  props: {
+    userName: String,
+    date: String,
+    year: Number,
+  },
+};
+</script>
