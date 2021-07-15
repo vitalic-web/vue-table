bf42285f7d186b24ad2363f7c34c949b2017be7e
Vitaly <vitalic.web@gmail.com>
2021-07-15 10:26:52 +0200

    Создал компонент Main

---
+++ src/views/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <h1>Table</h1>
+    <Table />
+    <!-- <Table :tableData="tableData"/> -->
+  </main>
+</template>
+
+<script>
+import Table from '@/components/Table.vue';
+// import tableData from '@/utils/tableData';
+
+export default {
+  name: 'Main',
+  components: {
+    Table,
+  },
+  // data() {
+  //   return {
+  //     tableData,
+  //   };
+  // },
+};
+</script>
