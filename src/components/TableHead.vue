a4a6e4ade807c0de4fffe74f205852a403568285
Vitaly <vitalic.web@gmail.com>
2021-07-15 15:53:22 +0200

    Обновлен компонент заголовков таблицы

---
+++ src/components/TableHead.vue
@@ -0,0 +1,18 @@
+<template>
+  <tr>
+    <th>{{userNameTitle}}</th>
+    <th>{{dateTitle}}</th>
+    <th>{{yearTitle}}</th>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'TableHead',
+  props: {
+    userNameTitle: String,
+    dateTitle: String,
+    yearTitle: String,
+  },
+};
+</script>
