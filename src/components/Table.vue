0ff87786251ab34a26b7fe92805cc28588b34613
Vitaly <vitalic.web@gmail.com>
2021-07-15 10:27:27 +0200

    Создал компонент таблицы

---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <table>
+    <caption>{{ tableData.name }}</caption>
+    <tr>
+      <th v-for="item in tableData.head" :key="item">
+        <TableHeadItem :item="item"/>
+      </th>
+    </tr>
+  </table>
+</template>
+
+<script>
+import TableHeadItem from '@/components/TableHeadItem.vue';
+import tableData from '@/utils/tableData';
+
+export default {
+  name: 'Table',
+  components: {
+    TableHeadItem,
+  },
+  data() {
+    return {
+      tableData,
+    };
+  },
+  // computed: {
+  //   getTableHead() {
+  //     return this.tableData.head;
+  //   },
+  // },
+};
+</script>
