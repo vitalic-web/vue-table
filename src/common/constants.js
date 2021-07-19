88a155d1cb344d25ea7737fb34d0bc60c3bee030
Vitaly <vitalic.web@gmail.com>
2021-07-19 11:07:47 +0200

    Обновлены данные заголовков таблицы

---
+++ src/common/constants.js
@@ -0,0 +1,18 @@
+const tableNames = {
+  name: 'Users info',
+  head: {
+    userName: 'User Name',
+    email: 'Email',
+    name: 'Name',
+    surname: 'Surname',
+    birthday: 'Birthday',
+    age: 'Age',
+  },
+};
+
+const someData = [];
+
+export {
+  tableNames,
+  someData,
+};
