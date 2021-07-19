0e3caf7d20b5e3ce5d0caa3bd9738d2fe7294b68
Vitaly <vitalic.web@gmail.com>
2021-07-19 11:10:19 +0200

    Обновлены функции обновления и добавления нужных данных в таблицу

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,15 +1,41 @@
-const getISODate = (date) => date.split('.').reverse().join('-');
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+const getFullDate = (date, isTable) => {
+  const fullDate = date.split('.');
 
-const addYear = (obj) => {
-  const newObj = JSON.parse(JSON.stringify(obj));
-  newObj.data.forEach((item) => {
-    // eslint-disable-next-line no-param-reassign
-    item.year = new Date(getISODate(item.date)).getFullYear();
+  if (fullDate[2].length === 2) {
+    fullDate[2] = `19${fullDate[2]}`;
+  }
+
+  if (isTable) {
+    return fullDate.join('.');
+  } return fullDate.reverse().join('-');
+};
+
+const getUserAge = (date) => {
+  const userDate = new Date(getFullDate(date));
+  const currentDate = new Date();
+  let age = currentDate.getFullYear() - userDate.getFullYear();
+  const monthDiff = currentDate.getMonth() - userDate.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < userDate.getDate())) {
+    age--;
+  }
+
+  return age;
+};
+
+const addAge = (usersData) => {
+  const newObj = JSON.parse(JSON.stringify(usersData));
+  newObj.forEach((item) => {
+    item.person.birthday = getFullDate(item.person.birthday, true);
+    item.person.age = getUserAge(item.person.birthday);
   });
   return newObj;
 };
 
 export {
-  getISODate,
-  addYear,
+  getFullDate,
+  getUserAge,
+  addAge,
 };
